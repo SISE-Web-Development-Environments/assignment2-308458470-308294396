8702963067c78508503da875c7a0dd98947fa3d7
yaarm <yaarmaypaz@gmail.com>
2020-04-28 18:49:03 +0300

    adding registration, login and settings

--- app.js
+++ app.js
@@ -1,172 +1,156 @@
 var context;
-var shape = new Object();
-var board;
-var score;
-var pac_color;
-var start_time;
-var time_elapsed;
-var interval;
+
+var userCounter = 0;
+
+var keyUp;
+var keyDown;
+var keyLeft;
+var keyRight;
 
 $(document).ready(function() {
+	createUserDB();
+	//showWelcome();
 	context = canvas.getContext("2d");
-	Start();
+	//Start();
 });
 
-function Start() {
-	board = new Array();
-	score = 0;
-	pac_color = "yellow";
-	var cnt = 100;
-	var food_remain = 50;
-	var pacman_remain = 1;
-	start_time = new Date();
-	for (var i = 0; i < 10; i++) {
-		board[i] = new Array();
-		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
-			if (
-				(i == 3 && j == 3) ||
-				(i == 3 && j == 4) ||
-				(i == 3 && j == 5) ||
-				(i == 6 && j == 1) ||
-				(i == 6 && j == 2)
-			) {
-				board[i][j] = 4;
-			} else {
-				var randomNum = Math.random();
-				if (randomNum <= (1.0 * food_remain) / cnt) {
-					food_remain--;
-					board[i][j] = 1;
-				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
-					shape.i = i;
-					shape.j = j;
-					pacman_remain--;
-					board[i][j] = 2;
-				} else {
-					board[i][j] = 0;
-				}
-				cnt--;
-			}
-		}
-	}
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
-	}
-	keysDown = {};
-	addEventListener(
-		"keydown",
-		function(e) {
-			keysDown[e.keyCode] = true;
-		},
-		false
-	);
-	addEventListener(
-		"keyup",
-		function(e) {
-			keysDown[e.keyCode] = false;
-		},
-		false
-	);
-	interval = setInterval(UpdatePosition, 250);
+function createUserDB() {
+	var names = [];
+	sessionStorage.setItem("names", JSON.stringify(names));
+	var storedNames = JSON.parse(sessionStorage.getItem("names"));
+	storedNames[userCounter++]= ({userName:"p", pass: "p"});
+	storedNames[userCounter++]= ({userName:"yaarm", pass: "a123456"});
+	sessionStorage.setItem("names", JSON.stringify(storedNames));
+	//register("p","p");
 }
 
-function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
-	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
+
+function register(userName, pass) {
+	alert("Im in register");
+	if (validRegisteration()) {
+		var storedNames = JSON.parse(sessionStorage.getItem("names"));
+		console.log(storedNames);
+		storedNames[userCounter++]= ({userName: userName, pass: pass});
+		sessionStorage.setItem("names", JSON.stringify(storedNames));
+		alert("The user added");
+		showWelcome();
 	}
-	return [i, j];
+	else {
+		alert("Not valid!!");
+	}
+	//showWelcome();
 }
 
-function GetKeyPressed() {
-	if (keysDown[38]) {
-		return 1;
-	}
-	if (keysDown[40]) {
-		return 2;
-	}
-	if (keysDown[37]) {
-		return 3;
-	}
-	if (keysDown[39]) {
-		return 4;
-	}
+//add function of validate to login page
+
+function checkUserInDB(username, passw) {
+	var storedNames = JSON.parse(sessionStorage.getItem("names"));
+	let result = storedNames.filter(user => {return user.userName == username && user.pass == passw})
+	if (result.length == 1) {
+		showSettings();
+	}	
 }
 
-function Draw() {
-	canvas.width = canvas.width; //clean board
-	lblScore.value = score;
-	lblTime.value = time_elapsed;
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
-			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
-			if (board[i][j] == 2) {
-				context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 1) {
-				context.beginPath();
-				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 4) {
-				context.beginPath();
-				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
-				context.fill();
-			}
-		}
+function startNewGame(numBalls, color1, color2, color3, time, numMonsters) {
+	if (validSettings()){
+		showGame(keyUp, keyDown, keyLeft, keyRight, numBalls, numMonsters, color1, color2, color3, context);
+		Start();
+		alert("Start the game");
+	}
+
+}
+
+function randomSettings() {
+	setDefaultKeys();
+	document.getElementById("keyUpPressed").value = "ArrowUp";
+	document.getElementById("keyDownPressed").value = "ArrowDown";
+	document.getElementById("keyLeftPressed").value = "ArrowLeft";
+	document.getElementById("keyRightPressed").value = "ArrowRight";
+	document.getElementById("numBalls").value = Math.floor(Math.random() * 40) + 50;
+	document.getElementById("time").value = Math.floor(Math.random() * 30) + 60;
+	document.getElementById("monsters").value = (Math.floor(Math.random() * 3) + 1).toString();
+	document.getElementById("color1").value = "Blue";
+	document.getElementById("color2").value = "Red";
+	document.getElementById("color3").value = "Green";
+}
+
+function setDefaultKeys() {
+	keyUp = "ArrowUp";
+	keyDown = "ArrowDown";
+	keyLeft = "ArrowLeft";
+	keyRight = "ArrowRight";
+}
+
+function keyPressed(event) {
+	var type = event.target;
+	switch(type.name) {
+		case "keyUp":
+			keyUp = event.key;
+			document.getElementById("keyUpPressed").value = keyUp;
+			break;
+		case "keyDown":
+			keyDown = event.key;
+			$("#keyDown").value = keyDown;
+			document.getElementById("keyDownPressed").value = keyDown;
+			break;
+		case "keyLeft":
+			keyLeft = event.key;
+			$("#keyLeft").value = keyLeft;
+			document.getElementById("keyLeftPressed").value = keyLeft;
+			break;
+		case "keyRight":
+			keyRight = event.key;
+			$("#keyRight").value = keyRight;
+			document.getElementById("keyRightPressed").value = keyRight;
+			break;
 	}
 }
 
-function UpdatePosition() {
-	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
-	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
-			shape.j--;
-		}
-	}
-	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
-			shape.j++;
-		}
-	}
-	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
-			shape.i--;
-		}
-	}
-	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
-			shape.i++;
-		}
-	}
-	if (board[shape.i][shape.j] == 1) {
-		score++;
-	}
-	board[shape.i][shape.j] = 2;
-	var currentTime = new Date();
-	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
-	} else {
-		Draw();
-	}
+function showWelcome() {
+	$(".welcome").show();
+	$(".signUp").hide();
+	$(".login").hide();
+	$(".board").hide();
 }
+
+function showSignUp(){
+	$(".signUp").show();
+	$(".welcome").hide();
+	$(".login").hide();
+	$(".board").hide();
+	$(".about").hide();
+}
+
+function showLogin(){
+	$(".login").show();
+	$(".welcome").hide();
+	$(".signUp").hide();
+	$(".board").hide();
+	$(".about").hide();
+}
+
+function showAbout(){
+	$(".about").show();
+	$(".welcome").hide();
+	$(".login").hide();
+	$(".signUp").hide();
+	$(".board").hide();
+}
+
+function showSettings(){
+	$(".settings").show();
+	$(".about").hide();
+	$(".welcome").hide();
+	$(".login").hide();
+	$(".signUp").hide();
+	$(".board").hide();
+}
+
+function showGame() {
+	$(".welcome").hide();
+	$(".signUp").hide();
+	$(".login").hide();
+	$(".board").show();
+	$(".settings").hide();
+}
+
---
+++ resources/js/validate.js
@@ -0,0 +1,202 @@
+
+function validRegisteration() {
+
+    $("#signUpForm").validate({
+        rules: {
+            userName: {
+                required: true,
+                minlength: 1
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                legalPassword: true,
+            },
+            firstName: {
+                required: true,
+                minlength: 1,
+                legalName: true
+            },
+            lastName: {
+                required: true,
+                minlength: 1,
+                legalName: true
+            },
+            email: {
+                required: true,
+                email: true,
+                legalEmail: true
+            },
+            date: {
+                required: true,
+            }
+        },
+        messages: {
+            userName: {
+                required: "Plase provide a userName",
+                minlength: "Your userName must be at least 1 character long."
+            },
+            password: {
+                required: "Plase provide a password",
+                minlength: "Your password must be at least 6 characters long."
+            },
+            fullName: {
+                required: "Plase provide a full name",
+                minlength: "Your full name must be at least 1 character long."
+            },
+            lastName: {
+                required: "Plase provide a last ame",
+                minlength: "Your last name must be at least 1 character long."
+            },
+            email: {
+                required: "Plase provide an email",
+                email: "Your email is not valid."
+            },
+            date: {
+                required: "Plase provide a birthdate",
+            }
+        }
+
+    })
+    return $("#signUpForm").valid();
+
+};
+
+$.validator.addMethod("legalPassword", function(value, element) {
+    return this.optional( element ) || /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/.test( value );
+}, "Your password should contains letters and numbers.");
+
+$.validator.addMethod("legalName", function(value, element) {
+    return this.optional( element ) || /^[a-zA-Z]+$/.test( value );
+}, "Your name should contains only letters.");
+
+$.validator.addMethod("legalEmail", function(value, element) {
+    return this.optional( element ) || /\S+@\S+\.\S+/.test( value );
+}, "Your email is not valid.");
+
+/*
+$.validator.addMethod("legalDate", function() {
+    var dateString = $("#birthdate").value;
+    var birthDate = new Date(dateString);
+    var today = new Date();
+    if ( birthDate > today ) { 
+        $('#birthdate').after('<p>You cannot enter a date in the future!.</p>');
+        return false;
+    }
+    return true;
+});
+*/
+
+function validSettings() {
+
+    $("#settingsForm").validate({
+        rules: {
+            keyUp: {
+                required: true
+            },
+            keyDown: {
+                required: true
+            },
+            keyLeft: {
+                required: true
+            },
+            keyRight: {
+                required: true
+            },
+            numBalls: {
+                required: true,
+                min: 50,
+                max: 90
+            },
+            color1: {
+                required: true,
+                color1NotChosen: true,
+                diffColors: true
+            },
+            color2: {
+                required: true,
+                color2NotChosen: true
+            },
+            color3: {
+                required: true,
+                color3NotChosen: true
+            },
+            time: {
+                required: true,
+                min: 60
+            },
+            monsters: {
+                required: true,
+                monsterNotChosen: true
+            }
+        
+        },
+        messages: {
+            keyUp: {
+                required: "Please press a key"
+            },
+            keyDown: {
+                required: "Please press a key"
+            },
+            keyLeft: {
+                required: "Please press a key"
+            },
+            keyRight: {
+                required: "Please press a key"
+            },
+            numBalls: {
+                required: "Please choose number of balls",
+                min: "The minumun number of balls is 60",
+                max: "The minumun number of balls is 90"
+            },
+            color1: {
+                required: "Please choose the first color",
+                diffColors: "Please choose differnt colors"
+            },
+            color2: {
+                required:  "Please choose the first color"
+            },
+            color3: {
+                required:  "Please choose the first color",
+            },
+            time: {
+                required:  "Please define the time",
+                min:  "The minumun time is 60 seconds"
+            },
+            monsters: {
+                required:  "Please choose the number of monsters"
+            }
+        }
+
+    })
+    return $("#settingsForm").valid();
+
+};
+
+$.validator.addMethod("diffColors", function(value, element) {
+    var color1 = $("#Color1").value;
+    var color2 = $("#Color2").value;
+    var color3 = $("#Color3").value;
+
+    return color1 != color2 && color1 != color3 && color2 != color3;
+}, "Please choose different colors");
+
+$.validator.addMethod("color1NotChosen", function(value, element) {
+    var color = $("#Color1").value;
+    return color != "";
+}, "Please choose the first color");
+
+$.validator.addMethod("color2NotChosen", function(value, element) {
+    var color = $("#Color2").value;
+    return color != "";
+}, "Please choose the second color");
+
+$.validator.addMethod("color3NotChosen", function(value, element) {
+    var color = $("#Color3").value;
+    return color != "";
+}, "Please choose the third color");
+
+$.validator.addMethod("monsterNotChosen", function(value, element) {
+    var monster = $("#monster").value;
+    return monster != "";
+}, "Please choose the number of monsters");
