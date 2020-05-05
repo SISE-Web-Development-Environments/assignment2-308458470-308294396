856187d6b14fa4ce0bcf7a856ac48b4a358ee2e9
yaarm <yaarmaypaz@gmail.com>
2020-05-05 13:11:12 +0300

    fixing date validation

--- resources/js/validate.js
+++ resources/js/validate.js
@@ -27,7 +27,7 @@ function validRegisteration() {
                 email: true,
                 legalEmail: true
             },
-            dateOfBirth: {
+            date: {
                 required: true,
             }
         },
@@ -52,7 +52,7 @@ function validRegisteration() {
                 required: "Plase provide an email",
                 email: "Your email is not valid."
             },
-            dateOfBirth: {
+            date: {
                 required: "Plase provide a birthdate",
             }
         }
